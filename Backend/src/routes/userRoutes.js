68c92567ca3f004aa4f7fa0531b2d5f7bde8ef3f
ashwanisingh011 <ashwanisingh9737@gmail.com>
2026-03-18 12:41:48 +0530

    working proptotype not integrated AI yet

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import { loginUser, getUserProfile, updateProgress, getLeaderboard } from '../controllers/userController.js';
+import { getUserProfile, updateProgress, getLeaderboard } from '../controllers/userController.js';
 
 const router = express.Router();
 
-router.post('/login', loginUser);
 router.get('/leaderboard', getLeaderboard);
 router.get('/:username', getUserProfile);
 router.post('/progress', updateProgress);
